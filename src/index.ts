5a41528ad643588acf5105051cf39485f3a641c6
alexanderpo <alexander.pozhidaev@gmail.com>
2022-12-18 16:33:51 +0100

    fix: error The JSX syntax extension is not currently enabled

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,42 @@
-import LitePicker from "./LitePicker";
-export default LitePicker;
+import {
+  memo,
+  useRef,
+  forwardRef,
+  useLayoutEffect,
+  useImperativeHandle,
+  InputHTMLAttributes,
+  createElement,
+} from "react";
+import Litepicker from "litepicker";
+
+type Props = InputHTMLAttributes<HTMLInputElement> & {
+  options: any;
+};
+
+const LitePicker = forwardRef(function LitePicker(props: Props, ref) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  useImperativeHandle(ref, () => inputRef.current);
+  const { options, ...rest } = props;
+
+  useLayoutEffect(() => {
+    const element: any = inputRef.current;
+    if (element) {
+      if (element.litePickerInstance) {
+        element.litePickerInstance.destroy();
+      }
+      element.value = "";
+      element.litePickerInstance = new Litepicker({
+        ...props.options,
+        element,
+      });
+    }
+  }, [options]);
+
+  return createElement("input", {
+    ...rest,
+    ref: inputRef,
+    type: "text",
+  });
+});
+
+export default memo(LitePicker);
